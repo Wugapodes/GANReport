ede93e185d14bdcdee67122d586ab6a0aef51fcd
Wugapodes <wugapodes@aol.com>
2016-12-08 16:52:12 -0500

    fixing major regex bug (thanks bluemoonset)

--- GANReportBot.py
+++ GANReportBot.py
@@ -71,7 +71,7 @@ page = pywikibot.Page(site,'Wikipedia:Good article nominations')
 
 #Compile regexes
 ##Finds GAN entries and returns time stamp, title, and the following line
-entRegex = re.compile(r'\{\{GANentry.*?\|1\=(.*?)\|.*?(\d\d\:\d\d, \d+ .*? \d\d\d\d) \(UTC\)\n(.*)\n')
+entRegex = re.compile(r'\{\{GANentry.*?\|1\=(.*?)\|.*?(\d\d\:\d\d, \d+ .*? \d\d\d\d) \(UTC\)(?=\n(.*?)\n)')
 ##Finds the Wikipedia UTC Timestamp
 datRegex = re.compile(r', (\d+) (.*?) (\d\d\d\d)')
 
